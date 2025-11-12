efd0c09e745aca6a73760764b3ee5f6b4f1e2b2e
MinQDU <2025014@daelim.ac.kr>
2025-11-12 10:22:03 +0900

    profile -> profile77

--- src/main/kotlin/company/project/lib/common/objects/ApiPaths.kt
+++ src/main/kotlin/company/project/lib/common/objects/ApiPaths.kt
@@ -25,7 +25,7 @@ object ApiPaths {
 
 	// user
 	private const val USER = "$APP/user"
-	const val PROFILE = "$USER/profile"
+	const val PROFILE = "$USER/profile77"
 	const val SIGN_UP = "$USER/sign-up"
 	const val LOGIN = "$USER/login"
 	const val REFRESH_TOKEN = "$USER/refresh-token"
